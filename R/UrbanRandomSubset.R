285fd6fa1f1572f4d13800628ad8869f037d52b4
dzhakparov <66740648+dzhakparov@users.noreply.github.com>
2023-06-09 09:31:26 +0200

    uploaded sample data

---
+++ R/UrbanRandomSubset.R
@@ -0,0 +1,12 @@
+#' SOS-ALL dataset Urban subset
+#'
+#' A dataset containing 1000 randomly selected columns from the complete SOS-ALL
+#' dataset.
+#'
+#' @format A data frame with 60 rows and 1001 columns:
+#' \describe{
+#'   \item{var1}{Description of var1}
+#'   \item{var2}{Description of var2}
+#'   ...
+#' }
+"UrbanRandomSubset"
