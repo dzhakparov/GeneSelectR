e417a4040072619063284414d9f032b8cc9c8888
dzhakparov <66740648+dzhakparov@users.noreply.github.com>
2023-06-12 17:25:32 +0200

    stable solution for the onLoad function

---
+++ R/configure_environment.R
@@ -0,0 +1,107 @@
+#' Configure Python Environment for GeneSelectR
+#'
+#' This function checks if Conda is installed, creates a new Conda environment (if it does not already exist),
+#' installs necessary Python packages into the environment, and sets it as the active environment for reticulate.
+#'
+#' @param env_name The name of the Conda environment to be created. Defaults to "GeneSelectR_env".
+#'
+#' @return A message indicating the status of the environment configuration. If the configuration is successful,
+#' the function will prompt the user to restart their R session for the changes to take effect.
+#'
+#' @examples
+#' \dontrun{
+#' # Configure the default environment
+#' configure_environment()
+#'
+#' # Configure a custom environment
+#' configure_environment("my_env_name")
+#' }
+#'
+#' @export
+configure_environment <- function(env_name = "GeneSelectR_env") {
+  # Check if Conda is installed
+  if (is.null(reticulate::conda_version())) {
+    stop("Conda is not installed on your system. Please install Conda before proceeding.")
+  } else {
+    message("Conda is installed.")
+  }
+
+  # Check if the conda environment exists
+  envs <- reticulate::conda_list()
+  if (env_name %in% envs$name) {
+    message(paste("The conda environment", env_name, "already exists. Skipping environment creation and package installation."))
+  } else {
+    # Ask the user if they want to create the environment
+    if (interactive() &&
+        !utils::menu(c("yes", "no"),
+                     title = paste("The conda environment", env_name, "does not exist. Do you want to create it?")) == 1) {
+      stop(paste("The conda environment", env_name, "was not created."))
+    }
+    # Create the conda environment
+    reticulate::conda_create(env_name)
+
+    # Ask the user if they want to install the necessary Python packages
+    if (interactive() &&
+        !utils::menu(c("yes", "no"),
+                     title = "Do you want to install the necessary Python packages to the environment?") == 1) {
+      stop("The necessary Python packages were not installed.")
+    }
+
+    # Install the necessary Python packages
+    python_packages = c("scikit-learn",
+                        "pandas",
+                        "numpy <= 1.19",
+                        "lightgbm",
+                        "py-xgboost",
+                        "boruta_py")
+
+    reticulate::conda_install(packages = python_packages,
+                              envname = env_name,
+                              channel = 'conda-forge')
+  }
+
+  message("Please restart your R session for the changes to take effect.")
+}
+
+#' Set RETICULATE_PYTHON for the Current Session
+#'
+#' This function sets the RETICULATE_PYTHON environment variable to the path of the Python interpreter
+#' in the specified Conda environment, but only for the current R session. The change will not persist
+#' after the R session is closed.
+#'
+#' @param env_name The name of the Conda environment. Default is 'GeneSelectR_env'.
+#'
+#' @details
+#' This function checks if the specified Conda environment exists. If it does, the function sets the
+#' RETICULATE_PYTHON environment variable to the path of the Python interpreter in that environment.
+#' If the environment does not exist, the function stops with an error message.
+#'
+#' Users need to run this function in every new R session where they want to use your package. Also,
+#' they should run this function before loading your package with library(), because the RETICULATE_PYTHON
+#' environment variable needs to be set before reticulate is loaded.
+#'
+#' @return
+#' This function does not return a value. It sets the RETICULATE_PYTHON environment variable for the
+#' current R session and prints a message indicating the new value of RETICULATE_PYTHON.
+#'
+#' @examples
+#' \dontrun{
+#' set_reticulate_python('GeneSelectR_env')
+#' }
+#' @export
+set_reticulate_python <- function(env_name = 'GeneSelectR_env') {
+  # Check if the conda environment exists
+  envs <- reticulate::conda_list()
+  if (!(env_name %in% envs$name)) {
+    stop(paste("The conda environment", env_name, "does not exist. Please create it before proceeding."))
+  }
+
+  # Get the path to the Python interpreter in the environment
+  python_path <- envs[envs$name == env_name, "python"]
+
+  # Set the RETICULATE_PYTHON environment variable for the current session
+  Sys.setenv(RETICULATE_PYTHON = python_path)
+
+  message(paste("Set RETICULATE_PYTHON to", python_path, "for the current R session."))
+}
+
--- R/zzz.R
+++ R/zzz.R
@@ -42,50 +42,10 @@ multiprocessing <- NULL
 #'
 #' @name onLoad
 #' @keywords internal
-.onLoad <- function(libname, pkgname) {
-  # Check if Conda is installed
-  if (is.null(reticulate::conda_version())) {
-    stop("Conda is not installed on your system. Please install Conda before proceeding.")
-  } else {
-    message("Conda is installed.")
-  }
+#'
+#'
 
-  # Check if the conda environment "GeneSelectR_env" exists
-  envs <- reticulate::conda_list()
-  if ("GeneSelectR_env" %in% envs$name) {
-    message("The conda environment 'GeneSelectR_env' already exists. Skipping environment creation and package installation.")
-  } else {
-    # Ask the user if they want to create the environment
-    if (interactive() &&
-        !utils::menu(c("yes", "no"),
-                     title = "The conda environment 'GeneSelectR_env' does not exist. Do you want to create it?") == 1) {
-      stop("The conda environment 'GeneSelectR_env' was not created.")
-    }
-    # Create the conda environment
-    reticulate::conda_create("GeneSelectR_env")
-
-  # Ask the user if they want to install the necessary Python packages
-  if (interactive() &&
-      !utils::menu(c("yes", "no"),
-                   title = "Do you want to install the necessary Python packages to the 'GeneSelectR_env' environment?") == 1) {
-    stop("The necessary Python packages were not installed.")
-  }
-
-  # Install the necessary Python packages
-  python_packages = c("scikit-learn",
-                      "pandas",
-                      "numpy <= 1.19",
-                      "lightgbm",
-                      "py-xgboost",
-                      "boruta_py")
-
-  reticulate::conda_install(packages = python_packages,
-                            envname = 'GeneSelectR_env',
-                            channel = 'conda-forge')
-}
-  # Use the conda environment
-  reticulate::use_condaenv("GeneSelectR_env")
-
+load_python_packages <- function() {
   # Import the Python modules with delay_load = TRUE
   sklearn <<- reticulate::import("sklearn", delay_load = TRUE)
   pandas <<- reticulate::import("pandas", delay_load = TRUE)
@@ -95,15 +55,18 @@ multiprocessing <- NULL
   sys <<- reticulate::import("sys", delay_load = TRUE)
   multiprocessing <<- reticulate::import("multiprocessing", delay_load = TRUE)
 
-  # # Update executable path in multiprocessing module only if the operating system is Windows
-  # if (Sys.info()["sysname"] == "Windows") {
-  #   exe <- file.path(sys$exec_prefix, "pythonw.exe")
-  #   sys$executable <- exe
-  #   sys$`_base_executable` <- exe
-  #   multiprocessing$set_executable(exe)
-  # }
   message("Imported Python libraries with delay_load = TRUE")
 }
+
+.onLoad <- function(libname, pkgname) {
+  load_python_packages()
+}
+
+
+
+
+
+
 
 
 
