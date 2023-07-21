a22c17bb27b7924f34391c3bf0788bba224b407e
dzhakparov <66740648+dzhakparov@users.noreply.github.com>
2023-07-21 10:56:32 +0200

    Update overlap calculation
    Added a possibility to provide any custom lists

--- R/calculate_overlap_coefficients.R
+++ R/calculate_overlap_coefficients.R
@@ -1,36 +1,16 @@
 #' @title Calculate Overlap and Similarity Coefficients between Feature Lists
 #' @description This function calculates the Overlap, Jaccard, and Soerensen-Dice coefficients to quantify
-#' the similarity between feature lists.
+#' the similarity between feature lists. In addition to feature importance and permutation importance,
+#' you can provide a custom list of feature names to be included in the overlap calculation.
 #' @param pipeline_results A PipelineResults object containing the fitted pipelines, cross-validation results, selected features,
 #'   mean performance, and mean feature importances.
-#' @return A list of matrices showing the Overlap, Jaccard, and Soerensen-Dice coefficients for the feature lists for feature importance and permutation importance scores if they are provided
+#' @param custom_lists An optional named list of character vectors. Each character vector should contain feature names.
+#'   The names of the list will be used as names in the resulting overlap coefficient matrices.
+#' @return A list of lists of matrices showing the Overlap, Jaccard, and Soerensen-Dice coefficients for the feature lists.
+#'   This includes coefficients for feature importance, permutation importance (if present), and custom lists (if provided).
 #' @importFrom tmod modOverlaps
 #' @export
-# calculate_overlap_coefficients <- function(pipeline_results) {
-#   # Check if input object belongs to the PipelineResults class
-#   if (!inherits(pipeline_results, "PipelineResults")) {
-#     stop("The input object does not belong to the PipelineResults class.")
-#   }
-#
-#   # Create feature lists
-#   feature_lists <- lapply(pipeline_results@mean_feature_importances, function(df) df$feature)
-#   names(feature_lists) <- names(pipeline_results@mean_feature_importances)
-#
-#   # Calculate the overlap coefficients and round to 2 decimal places
-#   calculate_coefficients <- function(stat) {
-#     coefficients <- tmod::modOverlaps(modules = feature_lists, mset = NULL, stat = stat)
-#     round(coefficients, 2)
-#   }
-#
-#   overlap.coef_features <- calculate_coefficients("overlap")
-#   j.coef_features <- calculate_coefficients("jaccard")
-#   s.coef_features <- calculate_coefficients("soerensen")
-#
-#   # Return the results as a list of matrices
-#   return(list(overlap = overlap.coef_features, jaccard = j.coef_features, soerensen = s.coef_features))
-# }
-
-calculate_overlap_coefficients <- function(pipeline_results) {
+calculate_overlap_coefficients <- function(pipeline_results, custom_lists = NULL) {
   # Check if input object belongs to the PipelineResults class
   if (!inherits(pipeline_results, "PipelineResults")) {
     stop("The input object does not belong to the PipelineResults class.")
@@ -39,7 +19,7 @@ calculate_overlap_coefficients <- function(pipeline_results) {
   # Function to create feature lists and calculate coefficients
   get_coefficients <- function(importances) {
     # Create feature lists
-    feature_lists <- lapply(importances, function(df) df$feature)
+    feature_lists <- lapply(importances, function(x) x)
     names(feature_lists) <- names(importances)
 
     # Calculate the overlap coefficients and round to 2 decimal places
@@ -56,16 +36,37 @@ calculate_overlap_coefficients <- function(pipeline_results) {
     return(list(overlap = overlap.coef_features, jaccard = j.coef_features, soerensen = s.coef_features))
   }
 
-  # Apply function to mean_feature_importances
-  feature_importance_coefficients <- get_coefficients(pipeline_results@mean_feature_importances)
+  # Extract feature names from mean_feature_importances
+  feature_importances_list <- lapply(pipeline_results@mean_feature_importances, function(df) df$feature)
 
   # Check if permutation_importances exist and apply function
   permutation_importance_coefficients <- NULL
   if (!is.null(pipeline_results@permutation_importances)) {
-    permutation_importance_coefficients <- get_coefficients(pipeline_results@permutation_importances)
+    # Extract feature names from permutation_importances
+    permutation_importances_list <- lapply(pipeline_results@permutation_importances, function(df) df$feature)
+  } else {
+    permutation_importances_list <- list()
   }
 
+  all_lists <- list("features" = feature_importances_list, "permutations" = permutation_importances_list)
+
+  if (!is.null(custom_lists)) {
+    all_lists <- lapply(all_lists, function(lst) c(lst, custom_lists))
+  }
+
+  result_lists <- lapply(all_lists, get_coefficients)
+
+  names(result_lists) <- c("feature_importance_coefficients", "permutation_importance_coefficients")
+
   # Return both lists of coefficients
-  return(list(feature_importance_coefficients = feature_importance_coefficients,
-              permutation_importance_coefficients = if (!is.null(permutation_importance_coefficients)) permutation_importance_coefficients  else list()))
+  return(result_lists)
 }
+
+
+
+
+
+
+
+
+
