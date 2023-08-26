663b57554ab5fd91d3ba0e2096871d9412ed3a47
dzhakparov <66740648+dzhakparov@users.noreply.github.com>
2023-08-26 21:26:25 +0200

    Perm imp absence error fixed

--- R/plot_feature_importance.R
+++ R/plot_feature_importance.R
@@ -16,54 +16,46 @@ plot_feature_importance <- function(pipelineresults, top_n_features = 10) {
   plots_list <- list()
 
   for(method_name in names(pipelineresults@inbuilt_feature_importance)){
-    # Extract inbuilt_feature_importance and permutation_importance from the PipelineResults object
+    # Extract inbuilt_feature_importance from the PipelineResults object
     mean_importance <- pipelineresults@inbuilt_feature_importance[[method_name]]
-    permutation_importance <- pipelineresults@permutation_importance[[method_name]]
 
     # Order by mean_importance and take top_n_features
     top_mean_features <- mean_importance %>%
       dplyr::arrange(dplyr::desc(mean_importance)) %>%
       dplyr::slice(1:top_n_features)
-    top_permutation_features <- permutation_importance %>%
-      dplyr::arrange(dplyr::desc(mean_importance)) %>%
-      dplyr::slice(1:top_n_features)
 
-    # Find the intersecting features
-    intersecting_features <- intersect(top_mean_features$feature, top_permutation_features$feature)
-
-    # Create a vector of colors only for intersecting features
-    color_vector <- if(length(intersecting_features) >= 3) {
-      setNames(RColorBrewer::brewer.pal(length(intersecting_features), "Set1"), intersecting_features)
-    } else {
-      setNames(grDevices::rainbow(length(intersecting_features)), intersecting_features)
-    }
-
-    # Create a new color column for both data frames. If the feature is not in intersecting_features, color it gray.
-    top_mean_features$color <- ifelse(top_mean_features$feature %in% intersecting_features, color_vector[as.character(top_mean_features$feature)], "purple")
-    top_permutation_features$color <- ifelse(top_permutation_features$feature %in% intersecting_features, color_vector[as.character(top_permutation_features$feature)], "lightgreen")
-
-    # Create the plots
+    # Create the plot for inbuilt feature importance
     p1 <- ggplot2::ggplot(top_mean_features, ggplot2::aes(x = stats::reorder(.data$feature, .data$mean_importance), y = .data$mean_importance)) +
-      ggplot2::geom_bar(stat = "identity", aes(fill = color)) +
-      ggplot2::scale_fill_identity() +
+      ggplot2::geom_bar(stat = "identity", fill = "purple") +
       ggplot2::coord_flip() +
       ggplot2::theme_minimal() +
       ggplot2::theme(axis.text.y = ggplot2::element_text(face = "bold")) +
       ggplot2::labs(x = "Features", y = "Importance", title = paste("Inbuilt Feature Importance for", method_name))
 
-    p2 <- ggplot2::ggplot(top_permutation_features, ggplot2::aes(x = reorder(.data$feature, .data$mean_importance), y = .data$mean_importance)) +
-      ggplot2::geom_bar(stat = "identity", aes(fill = color)) +
-      ggplot2::scale_fill_identity() +
-      ggplot2::coord_flip() +
-      ggplot2::theme_minimal() +
-      ggplot2::theme(axis.text.y = ggplot2::element_text(face = "bold")) +
-      ggplot2::labs(x = "Features", y = "Importance", title = paste("Permutation Feature Importance for", method_name))
+    # Check if permutation_importance is provided
+    if (!is.null(pipelineresults@permutation_importance[[method_name]])) {
+      permutation_importance <- pipelineresults@permutation_importance[[method_name]]
+      top_permutation_features <- permutation_importance %>%
+        dplyr::arrange(dplyr::desc(mean_importance)) %>%
+        dplyr::slice(1:top_n_features)
 
-    # Arrange the plots in a grid using plot_grid from the cowplot package
-    g <- cowplot::plot_grid(p1, p2, ncol = 2, labels = c("Inbuilt", "Permutation"))
+      # Create the plot for permutation feature importance
+      p2 <- ggplot2::ggplot(top_permutation_features, ggplot2::aes(x = reorder(.data$feature, .data$mean_importance), y = .data$mean_importance)) +
+        ggplot2::geom_bar(stat = "identity", fill = "lightgreen") +
+        ggplot2::coord_flip() +
+        ggplot2::theme_minimal() +
+        ggplot2::theme(axis.text.y = ggplot2::element_text(face = "bold")) +
+        ggplot2::labs(x = "Features", y = "Importance", title = paste("Permutation Feature Importance for", method_name))
+
+      # Arrange the plots in a grid using plot_grid from the cowplot package
+      g <- cowplot::plot_grid(p1, p2, ncol = 2, labels = c("Inbuilt", "Permutation"))
+    } else {
+      g <- p1
+    }
 
     plots_list[[method_name]] <- g
   }
 
   return(plots_list)
 }
+
