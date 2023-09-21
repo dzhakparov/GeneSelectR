1aee5f934dd6547178ad81b3989ba3c896f924ec
dzhakparov <66740648+dzhakparov@users.noreply.github.com>
2023-09-21 16:38:08 +0200

    Improved annotate_gene_lists

--- R/annotate_gene_lists.R
+++ R/annotate_gene_lists.R
@@ -7,13 +7,10 @@
 #' @param separator (optional) A character indicating the separator between ensembl_id and gene_name when format is "ensembl_gene_name".
 #' @return An object of the class AnnotatedGeneLists.
 #' @importFrom methods is
-#' @importFrom dplyr select inner_join rename coalesce
-#' @importFrom stringr str_split_fixed
-#' @importFrom magrittr %>%
-#' @importFrom rlang .data
+#' @importFrom clusterProfiler bitr
 #' @export
 #'
-annotate_gene_lists <- function(pipeline_results, custom_lists = NULL, annotations_ahb, format = c("ensembl_gene_name", "ensembl_id", "gene_name"), separator = "__") {
+annotate_gene_lists <- function(pipeline_results, custom_lists = NULL, annotations_ahb, format = c("entrezid", "ensembl_id", "gene_name")) {
 # Check if input object belongs to the PipelineResults class
 if (!inherits(pipeline_results, "PipelineResults")) {
   stop("The input object does not belong to the PipelineResults class.")
@@ -43,51 +40,40 @@ if (!is.null(custom_lists)) {
 annotated_mean_lists <- list()
 annotated_permutation_lists <- list()
 
-# Function to convert and annotate gene lists
-convert_and_annotate <- function(gene_lists, annotated_lists) {
-  # Convert each gene list and create AnnotatedGeneList object
-  for (method in names(gene_lists)) {
-    df <- data.frame(feature = gene_lists[[method]])
 
-    # Check format
-    if (format == "ensembl_gene_name") {
-      df[,c('ensembl_id', 'gene_name')] = stringr::str_split_fixed(df$feature, separator, n = Inf)
-      df <- df %>% dplyr::select(.data$ensembl_id, .data$gene_name)
-      df <- dplyr::inner_join(annotations_ahb, df, by = c('gene_id' = 'ensembl_id')) %>%
-        dplyr::mutate(gene_name = dplyr::coalesce(.data$gene_name.x, .data$gene_name.y)) %>%
-        dplyr::select(-.data$gene_name.x, -.data$gene_name.y)
-    } else if (format == "ensembl_id") {
-      df$ensembl_id = df$feature
-      df <- df %>% dplyr::select(.data$ensembl_id)
-      df <- dplyr::inner_join(annotations_ahb, df, by = c('gene_id' = 'ensembl_id')) %>%
-        dplyr::mutate(gene_name = dplyr::coalesce(.data$gene_name.x, .data$gene_name.y)) %>%
-        dplyr::select(-.data$gene_name.x, -.data$gene_name.y)
-    } else if (format == 'gene_name') { # gene_name
-      df$gene_name = df$feature
-      df <- df %>% dplyr::select(.data$gene_name)
-      df <- dplyr::inner_join(annotations_ahb, df, by = c('gene_name' = 'gene_name'))  %>%
-        dplyr::mutate(gene_name = dplyr::coalesce(.data$gene_name.x, .data$gene_name.y)) %>%
-        dplyr::select(-.data$gene_name.x, -.data$gene_name.y)
-    } else if (format == 'entrezid') {
-      df$entrezid = df$feature
-      df <- df %>% dplyr::select(.data$entrezid)
-      df <- dplyr::inner_join(annotations_ahb, df, by = c('entrezid' = 'entrezid'))  %>%
-        dplyr::mutate(gene_name = dplyr::coalesce(.data$gene_name.x, .data$gene_name.y)) %>%
-        dplyr::select(-.data$gene_name.x, -.data$gene_name.y)
+convert_and_annotate <- function(gene_lists, annotated_lists) {
+  for (method in names(gene_lists)) {
+    original_ids <- gene_lists[[method]]
+    # original_ids <- as.character(original_ids)
+
+    # Remove version numbers from Ensembl IDs if format is "ensembl_id"
+    if (format == "ensembl_id") {
+      original_ids <- gsub("\\..*", "", original_ids)
     }
 
-    # Rename the columns as required
-    df <- df %>%
-      dplyr::rename(ENSEMBL = .data$gene_id, SYMBOL = .data$gene_name, ENTREZID = .data$entrezid)
+    tryCatch({
+      # Convert IDs using bitr
+      if (format == "ensembl_id") {
+        converted_df <- clusterProfiler::bitr(original_ids, fromType = "ENSEMBL", toType = c("SYMBOL", "ENTREZID"), OrgDb = org.Hs.eg.db::org.Hs.eg.db)
+      } else if (format == "gene_name") {
+        converted_df <- clusterProfiler::bitr(original_ids, fromType = "SYMBOL", toType = c("ENSEMBL", "ENTREZID"), OrgDb = org.Hs.eg.db::org.Hs.eg.db)
+      } else if (format == "entrezid") {
+        converted_df <- clusterProfiler::bitr(original_ids, fromType = "ENTREZID", toType = c("SYMBOL", "ENSEMBL"), OrgDb = org.Hs.eg.db::org.Hs.eg.db)
+      }
 
-    # Create AnnotatedGeneList object
-    annotated_list <- new("GeneList",
-                          SYMBOL = as.character(df$SYMBOL),
-                          ENSEMBL = as.character(df$ENSEMBL),
-                          ENTREZID = as.character(df$ENTREZID))
+      # Create AnnotatedGeneList object
+      annotated_list <- new("GeneList",
+                            SYMBOL = as.character(converted_df$SYMBOL),
+                            ENSEMBL = as.character(converted_df$ENSEMBL),
+                            ENTREZID = as.character(converted_df$ENTREZID))
 
-    # Add to list of AnnotatedGeneList objects
-    annotated_lists[[method]] <- annotated_list
+      # Add to list of AnnotatedGeneList objects
+      annotated_lists[[method]] <- annotated_list
+
+    }, error = function(e) {
+      warning(paste("Failed to convert IDs for method:", method, "\nError:", e$message))
+      annotated_lists[[method]] <- NULL
+    })
   }
 
   return(annotated_lists)
