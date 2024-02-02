398247a91b3426dfc32889006214f7caf74274ca
Damir Zhakparov <66740648+dzhakparov@users.noreply.github.com>
2024-02-02 17:28:42 +0100

    Update working.R
    Draft update

--- inst/extras/working.R
+++ inst/extras/working.R
@@ -42,3 +42,75 @@ draw_heatmap <- function(data) {
 # Test the function with one of your matrices
 draw_heatmap(coeffs$feature_importance_coefficients$overlap)
 
+
+
+set.seed(123) # for reproducibility
+n_rows <- 10
+n_features <- 100
+
+# Randomly generate feature data
+X <- as.data.frame(matrix(rnorm(n_rows * n_features), nrow = n_rows, ncol = n_features))
+# Ensure each feature has a variance greater than 0.85
+for(i in 1:ncol(X)) {
+ while(var(X[[i]]) <= 0.85) {
+   X[[i]] <- X[[i]] * 1.1
+ }
+}
+colnames(X) <- paste0("Feature", 1:n_features)
+
+# Create a mock binary label column
+y <- factor(sample(c("Class1", "Class2"), n_rows, replace = TRUE))
+
+# set up the environment
+GeneSelectR::configure_environment()
+GeneSelectR::set_reticulate_python()
+
+# run GeneSelectR
+results <- GeneSelectR(X, y)
+
+
+# Perform gene selection and evaluation using user-defined methods
+fs_methods <- list("Lasso" = select_model(lasso(penalty = 'l1',
+                                                C = 0.1,
+                                                solver = 'saga'),
+                                          threshold = 'median'))
+custom_fs_grids <- list("Lasso" = list('C' = c(0.1, 1, 10)))
+results <- GeneSelectR(X,
+                       y,
+                       max_features = 15,
+                       custom_fs_methods = fs_methods,
+                       custom_fs_grids = custom_fs_grids)
+
+#' # Simple Usage with Mock Data
+#' # Create a mock PipelineResults object with minimal data
+mock_pipeline_results <- new("PipelineResults",
+                             inbuilt_feature_importance = list(
+                             "GeneSet1" = data.frame(feature = c("BRCA1", "TP53"))),
+                             permutation_importance = list(
+                               "GeneSet1" = data.frame(feature = c("BRCA1", "TP53"))))
+
+# Mock annotations data frame
+mock_annotations_ahb <- data.frame(gene_id = c("BRCA1", "TP53"),
+                                   gene_name = c("BRCA1", "TP53"),
+                                   entrezid = c(101, 102))
+
+# Convert and annotate gene lists
+annotated_lists <- annotate_gene_lists(mock_pipeline_results,
+                                       custom_lists = NULL,
+                                       mock_annotations_ahb,
+                                       "SYMBOL")
+print(annotated_lists)
+
+# Using Custom Gene Lists
+# Create custom gene lists
+custom_gene_lists <- list("CustomList1" = c("BRCA1", "TP53"))
+
+# Convert and annotate gene lists with custom gene lists included
+annotated_lists_custom <- annotate_gene_lists(mock_pipeline_results,
+                                              custom_gene_lists,
+                                              mock_annotations_ahb,
+                                              "SYMBOL")
+print(annotated_lists_custom)
+
+
+
